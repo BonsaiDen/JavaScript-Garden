37b7cb9096214a5ffd23e40c29e9ae229cc12d27
Tim Ruffles <timruffles@googlemail.com>
2013-10-24 16:50:39 +0100

    add favicon which handles subdomain

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@ git reset --hard master
 echo "Starting build"
 node build.js
 echo "Build complete"
-rm -rf `ls -d * | grep -vP 'site|node_modules|favicon.ico' | xargs`
+rm -rf `ls -d * | grep -vP 'site|node_modules' | xargs`
 echo "Cleaned out directory"
 mv site/* .
 if [[ $BUILD_ONLY ]]; then
